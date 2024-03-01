b2f579c9bf35305a8520036e0b3f35b6d2cf91e8
dereckabella <84955074+dereckabella@users.noreply.github.com>
2024-03-01 17:06:45 +0800

    f

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PhotoAlbum_Abella_Test_.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# PhotoAlbum_Abella
-A album of great games in every platform
